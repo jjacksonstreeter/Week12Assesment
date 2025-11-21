6ce7e68f9715aadbef555c23da0981de4c390d24
jjacksonstreeter <jjacksonstreeter@icstars.org>
2025-11-21 11:17:40 -0600

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+[
+    {
+        "ID": 1,
+        "Name": "Alice",
+        "Age": 25,
+        "JoinDate": "5/10/2021",
+        "Score": 88
+    },
+    {
+        "ID": 2,
+        "Name": "Bob",
+        "Age": null,
+        "JoinDate": "6/15/2021",
+        "Score": 90
+    }
+]
